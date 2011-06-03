4193bf36434c96f55c77a0f0fc93ebae475acc97
Vivek Shrivastava <vivek_shri@yahoo.com>
2011-06-03 13:38:05 +0530

    Added No-Arg Constructor For UserName
    Change-Id: Iff0cf5eaab54d32de120e7096d1aab672b6421c3

--- src/main/java/com/impetus/kundera/examples/entities/UserName.java
+++ src/main/java/com/impetus/kundera/examples/entities/UserName.java
@@ -17,52 +17,52 @@ import com.impetus.kundera.api.ColumnFamily;
  */
 @Entity
 @ColumnFamily(family = "Username", keyspace = "Examples")
-public class UserName implements Serializable
-{
-    private static final long serialVersionUID = 1L;   
-    
-    @Id    
-    private String userName;
-    
-    @Column(name = "id")
-    private String id;
-    
-    
-    public UserName(String userName, String id) {
-    	this.userName = userName;
-    	this.id = id;
-    }
+public class UserName implements Serializable {
+	private static final long serialVersionUID = 1L;
 
-    /**
-     * @return the id
-     */
-    public String getId()
-    {
-        return id;
-    }
+	@Id
+	private String userName;
 
-    /**
-     * @return the userName
-     */
-    public String getUserName()
-    {
-        return userName;
-    }
+	@Column(name = "id")
+	private String id;
 
-    /**
-     * @param id the id to set
-     */
-    public void setId(String id)
-    {
-        this.id = id;
-    }
+	public UserName() {
+		super();
+	}
 
-    /**
-     * @param userName the userName to set
-     */
-    public void setUserName(String userName)
-    {
-        this.userName = userName;
-    }
+	public UserName(String userName, String id) {
+		this.userName = userName;
+		this.id = id;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @return the userName
+	 */
+	public String getUserName() {
+		return userName;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @param userName
+	 *            the userName to set
+	 */
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
 }
